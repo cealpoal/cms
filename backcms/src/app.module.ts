680e641f8f602006fc53a001694eee18a064179d
cealpoal <cealpoal@gmail.com>
2023-05-06 12:16:33 -0500

    actualización para backend
    pendiente funcionamiento del encriptador

--- backcms/src/app.module.ts
+++ backcms/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MerlinController } from './merlin/merlin.controller';
+import { MerlinService } from './services/merlin/merlin.service';
+import { BaseService } from './services/base/base.service';
 
 @Module({
   imports: [],
   controllers: [AppController, MerlinController],
-  providers: [AppService],
+  providers: [AppService, MerlinService, BaseService],
 })
 export class AppModule {}
--- backcms/src/merlin/merlin.controller.ts
+++ backcms/src/merlin/merlin.controller.ts
@@ -1,39 +1,25 @@
 import { Body, Controller, Get, Param, Patch, Put } from '@nestjs/common';
-const fs = require('fs');
+import UsersRequest from 'src/dto/requests';
+import sessionRespose from 'src/dto/responses';
+import { MerlinService } from 'src/services/merlin/merlin.service';
 
 @Controller('merlin')
 export class MerlinController {
-    private ruta:string = './src/';
+    constructor(private merlin:MerlinService){}
 
-    @Get()
-    async abra(){
-        return this.ReadFile();
+    @Get('gateway')
+    async boo(@Body() data:UsersRequest):Promise<sessionRespose> {
+        this.merlin.Gateway(data);
+        return null;
     }
 
-    @Put(':fileName')
-    async kadabra(@Body() data: any, @Param("fileName") fileName: string ){
-        return this.WriteFile(fileName, data);
-    }
+    // @Get('/file')
+    // async abra(){
+    //     return this.ReadFile();
+    // }
 
-    private ReadFile():any{
-        try {
-            const data = fs.readFileSync(this.ruta + 'testJsonFile.json', 'utf8');
-            const jsonData = JSON.parse(data);            
-            return jsonData;
-        } catch (error) { // 7
-            console.log(`ERROR: ${error}`);
-            return null;
-        }
-    }
-
-    private WriteFile(fileName:string, data:any):boolean{
-        try {
-            const frameworksData = JSON.stringify(data);
-            fs.writeFileSync(this.ruta + fileName + '.json', frameworksData, 'utf-8');
-            return true;
-        } catch (error) { 
-            console.log(`WRITE ERROR: ${error}`);
-            return false;
-        }
-    }
+    // @Put('/file:fileName')
+    // async kadabra(@Body() data: any, @Param("fileName") fileName: string ){
+    //     return this.WriteFile(fileName, data);
+    // }
 }
