d5f5e5fe0d49763190a84627df7401781c4a3a5c
cealpoal <cealpoal@gmail.com>
2023-05-18 22:39:42 -0500

    Todo OK
    pendiente parte de carga de photos para galeria

--- cms/src/app/components/main/navi/navi.component.ts
+++ cms/src/app/components/main/navi/navi.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navi',
   templateUrl: './navi.component.html',
   styleUrls: ['./navi.component.css']
 })
-export class NaviComponent {
+export class NaviComponent implements OnInit {
+  private bodyElement:any;
+  show:boolean = false;
+  menu:any[] = [
+    {
+      path: '/news',
+      tool: 'Noticias',
+      icon: 'bx-news',
+      text: 'Noticias'
+    },
+    {
+      path: '/photos',
+      tool: 'Gallería',
+      icon: 'bx-photo-album',
+      text: 'Gallería'
+    },
+    {
+      path: '/contactus',
+      tool: 'Contáctenos',
+      icon: 'bxs-contact',
+      text: 'Contáctenos'
+    }
+  ];
+  
+  constructor(){
+    this.bodyElement = document.body;
+  }
 
+  ngOnInit(): void {    
+  }
+
+  showHide(){
+    this.show = !this.show;
+  }
+
+  changeMode(){
+    this.bodyElement.classList.toggle("dark");
+  }
 }
